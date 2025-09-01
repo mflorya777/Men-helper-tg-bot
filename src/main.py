76894e38766ac18b4580df4dfb9d6550b774159e
Mikhail Florya <floria.mahail@gmail.com>
2025-09-01 14:21:36 +0300

    created infrastructure code

--- src/main.py
+++ src/main.py
@@ -1,24 +1,34 @@
 import asyncio
 from os import getenv
+from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher
+from aiogram import (
+    Bot,
+    Dispatcher,
+)
 from aiogram.filters import Command
 from aiogram.types import Message
 
 
-TOKEN = getenv("BOT_TOKEN")
+load_dotenv()
 
+TOKEN = getenv("BOT_TOKEN")
 dp = Dispatcher()
 
 
-# Command handler
 @dp.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
+    """
+    Хэндлер старта бота.
+    """
     await message.answer("Привет! Я бот, сделанный на aiogram.")
 
 
-# Run the bot
+# Запуск бота
 async def main() -> None:
+    """
+    Функция запуска бота.
+    """
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 

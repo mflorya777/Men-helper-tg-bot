e412edcf8cf8c98eff53920f1cf5bbf541ee5a23
Mikhail Florya <floria.mahail@gmail.com>
2025-09-01 21:28:41 +0300

    refactor

--- src/main.py
+++ src/main.py
@@ -10,13 +10,8 @@ from aiogram import (
     Bot,
     Dispatcher,
 )
-from aiogram.filters import Command
-from aiogram.types import Message
 
-from src import (
-    texts,
-    keyboards,
-)
+from src.handlers import register_handlers
 
 
 _LOG = logging.getLogger("woman-tg-bot")
@@ -26,58 +21,11 @@ TOKEN = getenv("BOT_TOKEN")
 dp = Dispatcher()
 
 
-@dp.message(
-    Command("start"),
-)
-async def handler_start(
-    message: Message,
-) -> None:
-    """
-    Хэндлер старта бота.
-    """
-    await message.answer(
-        texts.start,
-        parse_mode="HTML",
-        reply_markup=keyboards.start_kb,
-    )
-
-
-@dp.message(
-    Command("about"),
-)
-async def handler_about_slash(
-    message: Message,
-) -> None:
-    """
-    Хэндлер со слэшем о боте: /about.
-    """
-    await message.answer(
-        texts.about_us,
-        parse_mode="HTML",
-        reply_markup=keyboards.start_kb,
-    )
-
-
-@dp.message(
-    lambda message: message.text == "ℹ️ Обо мне",
-)
-async def handler_about_button(
-    message: Message,
-) -> None:
-    """
-    Хэндлер для кнопки о боте.
-    """
-    await message.answer(
-        texts.about_us,
-        parse_mode="HTML",
-        reply_markup=keyboards.start_kb,
-    )
-
-
 async def main() -> None:
     """
     Функция запуска бота.
     """
+    await register_handlers(dp)
     bot = Bot(token=TOKEN)
     _LOG.info("Бот запущен")
     await dp.start_polling(bot)

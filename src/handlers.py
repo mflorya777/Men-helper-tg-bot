b6ba05805594d8f7d7f74ae2d59291dd73f466f0
Mikhail Florya <floria.mahail@gmail.com>
2025-09-02 14:11:57 +0300

    added fsm state for handler_start

--- src/handlers.py
+++ src/handlers.py
@@ -4,37 +4,56 @@ from aiogram import types
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram import Dispatcher
+from aiogram.fsm.context import FSMContext
 
 from src import (
     texts,
     keyboards,
 )
+from src.fsm_models.fsm_models import AgeConfirm
 
 
 _LOG = logging.getLogger("woman-tg-bot")
 
 
+
 async def handler_start(
     message: Message,
+    state: FSMContext,
 ) -> None:
     """
     Хэндлер старта бота.
     """
+    current_state = await state.get_state()
+    if current_state == AgeConfirm.confirmed:
+        # Уже подтверждал возраст, то сразу кидаем к девушкам
+        await send_girls(message)
+    else:
+        # Первый раз, то спрашиваем подтверждение
+        await message.answer(
+            texts.start,
+            parse_mode="HTML",
+            reply_markup=keyboards.confirm_kb,
+        )
+        await state.set_state(AgeConfirm.not_confirmed)
+
+
+async def send_girls(
+    message: Message,
+):
     await message.answer(
-        texts.start,
+        texts.girls,
         parse_mode="HTML",
-        reply_markup=keyboards.confirm_kb,
+        reply_markup=keyboards.girls_kb,
     )
 
 
 async def process_confirm_18(
     callback_query: types.CallbackQuery,
+    state: FSMContext,
 ):
-    await callback_query.message.answer(
-        texts.girls,
-        parse_mode="HTML",
-        reply_markup=keyboards.girls_kb,
-    )
+    await state.set_state(AgeConfirm.confirmed)  # сохраняем статус
+    await send_girls(callback_query.message)
     await callback_query.answer()
 
 
@@ -67,6 +86,9 @@ async def handler_about_button(
 async def register_handlers(
     dp: Dispatcher,
 ):
+    """
+    Функция регистрации всех хэндлеров.
+    """
     dp.message.register(
         handler_start,
         Command("start"),
